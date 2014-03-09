7512a463e667da619a2aff53035b90b9a56bf942
somename <someemail>
2014-03-09 18:47:59 -0300

    besouro automatic message

--- src/roman/Roman.java
+++ src/roman/Roman.java
@@ -18,6 +18,9 @@ public class Roman {
 		basicNumbers.put("III", 3);
 		basicNumbers.put("IV", 4);
 		basicNumbers.put("V", 5);
+		basicNumbers.put("VI", 6);
+		basicNumbers.put("VII", 7);
+		basicNumbers.put("VIII", 8);
 	}
 
 	public int intValue() {
