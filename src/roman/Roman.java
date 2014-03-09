9c5a771ad1574d185224d8adeae158dc06be4a8e
somename <someemail>
2014-03-09 18:35:25 -0300

    besouro automatic message

---
+++ src/roman/Roman.java
@@ -0,0 +1,15 @@
+package roman;
+
+public class Roman {
+
+	private int romanNumber;
+
+	public Roman(int romanNumber) {
+		this.romanNumber = romanNumber;
+	}
+
+	public int intValue() {
+		return 1;
+	}
+
+}
